a9cc3cbfb71252dec9ad3d4b319c2441f98e292d
ChromaEsport <max-kaya@live.fr>
2026-08-17 14:16:23 +0200

    Update espace-membre.js

--- espace-membre.js
+++ espace-membre.js
@@ -1442,10 +1442,6 @@ if (
     numeroMembre.textContent =
         "Communauté";
 
-    numeroMembreHeader.textContent =
-        "💜 COMMUNAUTÉ";
-
-
     statutAdhesion.textContent =
         "💜 Inscription gratuite";
 
